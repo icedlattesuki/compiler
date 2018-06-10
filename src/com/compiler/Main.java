23602df18cd0966803954f2bfab31866f88e8ed6
huangyusheng <huangyusheng@youzan.com>
2018-06-10 21:47:08 +0800

    modify Main

--- src/com/compiler/Main.java
+++ src/com/compiler/Main.java
@@ -1,25 +1,20 @@
 package com.compiler;
 
-import com.compiler.assemble.AssembleList;
-import com.compiler.assemble.AssembleTranslator;
 import com.compiler.cparser.Parser;
 import com.compiler.cparser.ast.Ast;
 import com.compiler.exception.SemanticError;
 import com.compiler.exception.SyntaxError;
-import com.compiler.ir.IrList;
-import com.compiler.ir.IrTranslator;
 import com.compiler.lexer.Lexer;
 import com.compiler.llvmir.LLVMIR;
 import com.compiler.semantic.SemanticAnalyzer;
 import java.io.FileReader;
-import java.io.PrintWriter;
 
 public class Main {
     public static void main(String[] args) {
         try {
             Parser parser = new Parser(new Lexer(new FileReader(args[0])));
             Ast ast = (Ast) parser.parse().value;
-//            ast.print();
+            ast.print();
             SemanticAnalyzer analyzer = new SemanticAnalyzer(ast);
             analyzer.analyse();
             LLVMIR irTranslator = new LLVMIR(ast);
